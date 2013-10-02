b360e4f48384524a8bf6711d94a5c7f7d9317acd
Robb Wright <siksia@gmail.com>
2013-10-02 11:32:25 -0700

    Added openshift secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,3 +1,4 @@
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # Be sure to restart your server when you modify this file.
 
 # Your secret key is used for verifying the integrity of signed cookies.
@@ -9,4 +10,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TurntableExporter::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+# Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
+TurntableExporter::Application.config.secret_token = initialize_secret(
+    :token,
+    '335a4e365ef2daeea969640d74e18f0e3cd9fae1abd8f4125691a880774ea6d456a29c0831aa6921bf86a710fe555e916f0673f5657619ec9df22e0409bec345'
+)
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # Be sure to restart your server when you modify this file.
 
-TurntableExporter::Application.config.session_store :cookie_store, key: '_turntable-exporter_session'
+TurntableExporter::Application.config.session_store :cookie_store, key: initialize_secret(
+    :session_store,
+    '_turntable-exporter_session'
+)
