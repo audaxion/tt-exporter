4f10fab5717fbde9bf7e0cd6b70ccc11c770f567
Robb Wright <siksia@gmail.com>
2013-10-02 11:10:46 -0700

    Openshift changes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ TurntableExporter::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   config.assets.compress = true
   # Compress JavaScripts and CSS.
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
 require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV['REDIS_URL'], :namespace => 'sidekiq'  }
+  config.redis = { :url => "redis://root:#{ENV['REDIS_PASSWORD']}@#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}/12", :namespace => 'sidekiq'  }
   config.server_middleware do |chain|
     chain.add Kiqstand::Middleware
   end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => ENV['REDIS_URL'], :namespace => 'sidekiq' }
+  config.redis = { :url => "redis://root:#{ENV['REDIS_PASSWORD']}@#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}/12", :namespace => 'sidekiq' }
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
