b0d6af624c343a3042d3b9c23fc076cbc32dcf42
Robb Wright <siksia@gmail.com>
2013-10-03 15:13:38 -0700

    Config comments

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 if Rails.env == 'production'
+  puts "Connecting to production redis"
   $redis = Redis.new(:path => "#{ENV['OPENSHIFT_DATA_DIR']}redis/redis.sock")
 else
+  puts "Connecting to dev/test redis"
   $redis = Redis.new(:url => ENV['REDIS_URL'])
 end
